817d945791f5b1c6b34378ea3f61750c384fd852
Jake Timmerman <jaketimm.01@gmail.com>
2025-06-24 09:54:20 -0400

    write empty file from main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import logging
 from racing_weather_api.config import LOG_FILE, LOG_LEVEL, LOG_FORMAT
 from racing_weather_api.data_processing.event_processing import get_events_with_weather
 
+import os
+import json
+
 # Configure logging
 logging.basicConfig(
     level=getattr(logging, LOG_LEVEL),
@@ -23,6 +26,26 @@ try:
     else:
         logger.warning("No events found for the current weekend")
     
+    #create an empty file for testing no events logic on website
+
+    # Define the path and filename
+    file_path = os.path.join('/', 'var', 'www', 'html', 'data', 'empty.json')
+
+    # Ensure the directory exists
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+    # Define the JSON data
+    data = {
+        "2025-06-13": []
+    }
+
+    # Write the JSON data to the file
+    with open(file_path, 'w') as json_file:
+        json.dump(data, json_file, indent=2)
+
+    print(f"File created at: {file_path}")
+
+
 except Exception as e:
     logger.error(f"Error in scheduled job: {str(e)}")
 
